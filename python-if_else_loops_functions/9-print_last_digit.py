aea4a03bad0dedf334717695930d48cbab36019b
Bryan <bryanmartel.po@gmial.com>
2026-01-13 12:42:56 +0000

    fix Correct output - case: 98 with return

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    lastdigit = abs(number) % 10
-    print("{}".format(lastdigit), end='')
+    last_digit = abs(number) % 10
+    print("{}".format(last_digit), end="")
     return last_digit
