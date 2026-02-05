bcfcf96996e97896b762bb840003a707f48882da
Bryan <bryanmartel.po@gmial.com>
2026-02-05 10:17:36 +0000

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that difine a function to check class type"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is instence of a_class"""
+    return isinstance(obj, a_class)
