e7af034a6714fa270a7dfe0b1c7c8f4d674ad434
Bryan <bryanmartel.po@gmial.com>
2026-02-05 15:06:34 +0000

    fix doctest

--- python-inheritance/main.py
+++ python-inheritance/main.py
@@ -17,8 +17,6 @@ except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
-    bg.integer_validator("distance", -4)
+    bg.integer_validator()
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
-
-help(BaseGeometry)
