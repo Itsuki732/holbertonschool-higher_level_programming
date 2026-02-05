61bb6c205776b8ab9aa9962e77766db2bcc3739b
Bryan <bryanmartel.po@gmial.com>
2026-02-05 11:16:41 +0000

    fix Correct output - case: a = 1 inherits_from(a, int)

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Return True if obj is instence of a_class"""
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
