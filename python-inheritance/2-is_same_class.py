4d57429956d72e84da31395c11b1e35e8370c736
Bryan <bryanmartel.po@gmial.com>
2026-02-05 09:48:37 +0000

    fix doc

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 """Module that difine a function to check class type"""
 def is_same_class(obj, a_class):
+    """Return True if obj is exactly a instence of a_class"""
     return type(obj) is a_class
