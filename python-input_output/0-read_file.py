11fa1c4163fb156464c361e57da51ebb9cf3bd70
Bryan <bryanmartel.po@gmial.com>
2026-02-09 11:02:21 +0000

    fix pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module that provides a function to read a text file and print its content."""
+"""Module that provides a function to read a text file and print its content"""
+
+
 def read_file(filename=""):
-    """Reads a text file and prints it to standard output."""
-    with open(filename, encoding="UTF-8") as file :
+    """Reads a text file and prints it to standard output"""
+    with open(filename, encoding="UTF-8") as file:
         my_file_0 = file.read()
         print(my_file_0, end="")
