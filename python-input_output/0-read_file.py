b1ae5a9ada746c3e7b97e23aa45a4953655c1774
Bryan <bryanmartel.po@gmial.com>
2026-02-09 11:01:09 +0000

    fix \n en tros

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,4 +4,4 @@ def read_file(filename=""):
     """Reads a text file and prints it to standard output."""
     with open(filename, encoding="UTF-8") as file :
         my_file_0 = file.read()
-        print(my_file_0)
+        print(my_file_0, end="")
