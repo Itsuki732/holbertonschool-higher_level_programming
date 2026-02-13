141b761b137ca315398e6292ee1b604cd971fdbc
Bryan <bryanmartel.po@gmial.com>
2026-02-13 13:49:12 +0000

    task 11 first commit

---
+++ python-input_output/11-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student class.
+"""
+
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list): List of attributes to retrieve
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        if isinstance(attrs, list) and all(
+                isinstance(attr, str) for attr in attrs):
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+            }
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replace all attributes of the Student instance.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
